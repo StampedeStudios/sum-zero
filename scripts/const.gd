ab6134a93b59e3236caa2da08acd7aa273a38e6d
Lincoln-Ab <abramodipietro@libero.it>
2024-08-20 12:52:33 +0200

    Update const.gd

--- scripts/const.gd
+++ scripts/const.gd
@@ -1,6 +1,4 @@
 class_name GlobalConst
 
-const UI_MIN_WIDTH: float = 350
-const UI_MAX_WIDTH: float = 650
 const CELL_SIZE: float = 128
-const HANDLE_SIZE: float = 20
+const HANDLE_SIZE: float = 30
