749bc7028b424f8820977065eff3f7bb0dcfca02
simone-lungarella <simonelungarella@gmail.com>
2024-08-20 08:59:01 +0200

    feat: added question mark texture and simple sound effect

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -10,6 +10,8 @@ signal level_loading(level_data: LevelData)
 signal level_start
 signal level_end
 
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
 
 func level_complete() -> void:
 	level_end.emit()
@@ -18,7 +20,10 @@ func level_complete() -> void:
 
 func load_level() -> void:
 	if current_level < level_data.size():
+		audio_stream_player_2d.play()
 		level_loading.emit(level_data[current_level])
-		level_size = level_data[current_level].cells_values[0].size() * GlobalConst.CELL_SIZE + GlobalConst.CELL_SIZE / 2
+		level_size = (
+			level_data[current_level].cells_values[0].size() * GlobalConst.CELL_SIZE
+			+ GlobalConst.CELL_SIZE / 2
+		)
 		level_start.emit()
-		
