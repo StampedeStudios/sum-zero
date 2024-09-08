2704094682d49cb68051d36583a4723a58f4ab46
simone-lungarella <simonelungarella@gmail.com>
2024-09-08 21:21:37 +0200

    fix: applied gdformat and gdlint rules

--- scripts/const.gd
+++ scripts/const.gd
@@ -1,16 +1,7 @@
 class_name GlobalConst
 
+enum AreaEffect { ADD, DOUBLE_ADD, SUBTRACT, DOUBLE_SUBTRACT, CHANGE_SIGN }
+enum AreaBehavior {}
+
 const CELL_SIZE: float = 256
 const HANDLE_SIZE: float = 128
-
-enum AreaEffect{
-	ADD,
-	DOUBLE_ADD,
-	SUBTRACT,
-	DOUBLE_SUBTRACT,
-	CHANGE_SIGN
-}
-
-enum AreaBehavior{
-	
-}
