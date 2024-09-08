68ac4bc72e84ea95b830cc1534493092951d36cb
Lincoln-Ab <abramodipietro@libero.it>
2024-09-08 20:43:18 +0200

    feat: slider upgrade independence

--- scripts/const.gd
+++ scripts/const.gd
@@ -2,3 +2,15 @@ class_name GlobalConst
 
 const CELL_SIZE: float = 256
 const HANDLE_SIZE: float = 128
+
+enum AreaEffect{
+	ADD,
+	DOUBLE_ADD,
+	SUBTRACT,
+	DOUBLE_SUBTRACT,
+	CHANGE_SIGN
+}
+
+enum AreaBehavior{
+	
+}
--- scripts/scalable_area.gd
+++ scripts/scalable_area.gd
@@ -6,63 +6,49 @@ signal scale_change
 const MINUS = preload("res://assets/ui/minus.png")
 const PLUS = preload("res://assets/ui/plus.png")
 
-var min_scale: float
-var area_limit: Vector2i
 var target_scale: float
 var fixed_scale: int
 var is_scaling: bool
 var _is_horizontal: bool
-var _extend_limit: int
 var _current_scale: int
 var _reachable_tiles: Array[Tile]
 var _area_increment: bool
 var _last_scale: int
+var _moves: int
+var _orientation: Vector2
 
 @onready var area: NinePatchRect = $Area
 @onready var handle = $Handle
 @onready var icon = $Icon
-
-
-func init(
-	is_horizontal: bool, extend_limit: int, reachable_tiles: Array[Tile], area_increment: bool
-) -> void:
+@onready var ray = $Ray
+	
+	
+func init(is_horizontal: bool, reachable_tiles: Array[Tile], area_increment: bool) -> void:
 	_is_horizontal = is_horizontal
-	_extend_limit = extend_limit
 	_reachable_tiles = reachable_tiles
 	_area_increment = area_increment
-	min_scale = GlobalConst.HANDLE_SIZE / GlobalConst.CELL_SIZE
-
-	if _is_horizontal:
-		if _extend_limit < 0:
-			rotation_degrees = 180
-			area_limit = Vector2i(_extend_limit, 0)
-		else:
-			area_limit = Vector2i(0, _extend_limit)
-	else:
-		if _extend_limit < 0:
-			rotation_degrees = 270
-			area_limit = Vector2i(_extend_limit, 0)
-		else:
-			rotation_degrees = 90
-			area_limit = Vector2i(0, _extend_limit)
 
 	icon.texture = PLUS if area_increment else MINUS
-
-
+	_orientation = Vector2(round(cos(self.rotation)), round(sin(self.rotation)))
+	
 func _process(_delta: float) -> void:
 	# inizio a scalare mentre il mouse è premuto
 	if is_scaling:
 		var tile_distance: float
+		var drag_direction: Vector2
 		if _is_horizontal:
-			tile_distance = (
-				(get_global_mouse_position().x - global_position.x) / GameManager.CELL_SIZE
-			)
+			tile_distance = get_global_mouse_position().x - global_position.x
+			drag_direction = Vector2(tile_distance, 0).normalized()
 		else:
-			tile_distance = (
-				(get_global_mouse_position().y - global_position.y) / GameManager.CELL_SIZE
-			)
-
-		target_scale = abs(clamp(tile_distance, area_limit.x, area_limit.y))
+			tile_distance = get_global_mouse_position().y - global_position.y
+			drag_direction = Vector2(0, tile_distance).normalized()
+		
+		if drag_direction == _orientation:
+			tile_distance = abs(tile_distance / GameManager.CELL_SIZE)
+		else:
+			tile_distance = 0
+			
+		target_scale = clamp(tile_distance, 0, _moves)
 		fixed_scale = round(target_scale)
 		_apply_scaling(target_scale)
 		_update_changed_tiles()
@@ -98,7 +84,7 @@ func _update_changed_tiles() -> void:
 
 
 func _apply_scaling(_new_scale: float) -> void:
-	area.size.x = (min_scale + _new_scale) * GlobalConst.CELL_SIZE
+	area.size.x = GlobalConst.HANDLE_SIZE + _new_scale * GlobalConst.CELL_SIZE
 	handle.position.x = _new_scale * GlobalConst.CELL_SIZE
 
 
@@ -106,5 +92,20 @@ func _on_handle_input_event(_viewport: Node, _event: InputEvent, _shape_idx: int
 	if _event is InputEventMouse:
 		if _event.is_action_pressed("click"):
 			area.material.set_shader_parameter("is_selected", true)
-			is_scaling = true
+			_check_limit()
 			_last_scale = _current_scale
+			is_scaling = true
+
+
+func _check_limit() -> void:
+	_moves = 0
+	ray.force_raycast_update()
+	while ray.is_colliding():
+		var tile: Tile = ray.get_collider().owner
+		if !tile.is_blocked:
+			_moves += 1
+			ray.add_exception( ray.get_collider() )
+			ray.force_raycast_update()
+		else:
+			break 		
+	ray.clear_exceptions()
