1fff239de8990a602cfe188fad14efe798df3cb6
Lincoln-Ab <abramodipietro@libero.it>
2024-10-20 21:01:15 +0200

    fix: dictionary bug order

---
+++ scripts/utility/level_container.gd
@@ -0,0 +1,30 @@
+class_name LevelContainer extends Resource
+
+@export var levels: Dictionary
+@export var levels_order: Array[String]
+
+
+func get_level_by_index(level_index: int) -> String:
+	return levels_order[level_index]
+
+
+func get_levels_group_by_index(pivot: int, group_size: int) -> Array[String]:
+	var levels_group: Array[String]
+	var last := mini(levels_order.size(), pivot + group_size)
+	for level_index in range(pivot, last):
+		levels_group.append(levels_order[level_index])
+	return levels_group
+
+
+func set_level(level_name: String, level_data: LevelData) -> void:
+	if !levels_order.has(level_name):
+		levels_order.append(level_name)
+	levels[level_name] = level_data
+
+
+func get_next_level(current_level_name: String) -> String:
+	for name_index in range(0, levels_order.size()):
+		if levels_order[name_index] == current_level_name:
+			if name_index + 1 < levels_order.size():
+				return levels_order[name_index + 1]
+	return ""		
--- scripts/utility/player_save.gd
+++ scripts/utility/player_save.gd
@@ -1,25 +1,45 @@
 class_name PlayerSave extends Resource
 
-@export var custom_levels: Dictionary
+@export var custom_levels: LevelContainer
 @export var custom_progress: Dictionary
 @export var persistent_progress: Dictionary
 
 
-func initialize_persistent_progress(save: PersistentSave) -> void:
-	for name in save.levels.keys():
-		if !persistent_progress.has(name):
-			persistent_progress[name] = LevelProgress.new()
-	if !persistent_progress.is_empty():
-		persistent_progress.values()[0].is_unlocked = true
+func initialize_player_save(save: LevelContainer) -> void:
+	# initialize custom level container on new savegame
+	if custom_levels == null:
+		custom_levels = LevelContainer.new()
+	# create new progress for each persistent level
+	for level_name in save.levels.keys():
+		if !persistent_progress.has(level_name):
+			persistent_progress[level_name] = LevelProgress.new()
+	# create new progress for each custom level
+	for level_name in custom_levels.levels.keys():
+		if !custom_progress.has(level_name):
+			var progress := LevelProgress.new()
+			progress.is_unlocked = true
+			progress.is_completed = true
+			progress.move_left = -1000
+			custom_progress[level_name] = progress
+	# unlock first main level
+	persistent_progress.get(save.get_level_by_index(0)).is_unlocked = true
 
 
-func reset_custom_progress(level_name: String) -> void:
-	var progress := LevelProgress.new()
-	progress.is_unlocked = true
-	progress.is_completed = true
-	progress.move_left = -100
-	custom_progress[level_name] = progress
+func reset_progress(group: GlobalConst.LevelGroup, level_name: String) -> void:
+	match group:
+		GlobalConst.LevelGroup.MAIN:
+			persistent_progress[level_name] = LevelProgress.new()
+		GlobalConst.LevelGroup.CUSTOM:
+			var progress := LevelProgress.new()
+			progress.is_unlocked = true
+			progress.is_completed = true
+			progress.move_left = -1000
+			custom_progress[level_name] = progress
 
-
-func reset_persistent_progress(level_name: String) -> void:
-	persistent_progress[level_name] = LevelProgress.new()
+			
+func unlock_level(group: GlobalConst.LevelGroup, level_name: String) -> void:
+	match group:	
+		GlobalConst.LevelGroup.MAIN:
+			persistent_progress.get(level_name).is_unlocked = true
+		GlobalConst.LevelGroup.CUSTOM:
+			custom_progress.get(level_name).is_unlocked = true
