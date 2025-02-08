00745af2d407a0f5b2d9d3b004424d9fc0a306cc
simone-lungarella <simonelungarella@gmail.com>
2025-02-08 11:29:58 +0100

    fix: applied formatter and linter

--- scripts/utility/randomizer_options.gd
+++ scripts/utility/randomizer_options.gd
@@ -2,16 +2,11 @@ class_name RandomizerOptions extends Resource
 
 @export_group("CELLS")
 
-
 @export_group("SLIDERS")
-## Sliders occupation rule 
-@export var OCCUPATION_RULES := {
-	"STANDARD": 70,
-	"LOWER": 10,
-	"UPPER": 20
-}
+## Sliders occupation rule
+@export var occupation_rules := {"STANDARD": 70, "LOWER": 10, "UPPER": 20}
 ## Standard occupancy by number of possible places
-@export var OCCUPATION_STD := {
+@export var occupation_std := {
 	4: Vector2i(2, 3),
 	6: Vector2i(3, 4),
 	8: Vector2i(3, 5),
@@ -23,23 +18,14 @@ class_name RandomizerOptions extends Resource
 	20: Vector2i(8, 14)
 }
 ## Lower subtract probability
-@export var OCCUPATION_LOWER := 30
+@export var occupation_lower := 30
 ## Upper addiction probability
-@export var OCCUPATION_UPPER := 60
+@export var occupation_upper := 60
 ## Sliders extension rule
-@export var EXTENSION_RULES := {
-	"MAX": 60,
-	"NONE": 10,
-	"RANDOM": 30
-}
+@export var extension_rules := {"MAX": 60, "NONE": 10, "RANDOM": 30}
 ## Slider full probability
-@export var FULL_ODD := 15
+@export var full_odd := 15
 ## Slider block full probability
-@export var BLOCK_FULL_ODD := 50
+@export var block_full_odd := 50
 ## Sliders type rules
-@export var TYPE_RULES := {
-	"ADD": 45,
-	"SUBTRACT": 45,
-	"CHANGE_SIGN": 3,
-	"BLOCK": 7
-}
+@export var type_rules := {"ADD": 45, "SUBTRACT": 45, "CHANGE_SIGN": 3, "BLOCK": 7}
