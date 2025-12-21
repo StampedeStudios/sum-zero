9db85f7722eb74deb649e69fd03cac60734e8bb9
Simone <simonelungarella@gmail.com>
2025-12-21 12:37:19 +0100

    fix: correct an issue that was causing classic mode to result as locked after completing all levels

--- scripts/user_interface/play_mode_ui.gd
+++ scripts/user_interface/play_mode_ui.gd
@@ -4,6 +4,7 @@ const LOCKED_MODE = "res://assets/ui/locked_mode.png"
 const COMPLETED_MODE = "res://assets/ui/completed_mode.png"
 
 var _is_locked := false
+var _is_completed := false
 
 @onready var mode_texture: TextureRect = %ModeTexture
 @onready var overlay_texture: TextureRect = %OverlayTexture
@@ -15,18 +16,24 @@ func setup(mode: PlayMode) -> void:
 
 	match mode.unlock_mode:
 		PlayMode.UnlockMode.NONE:
-			pass
+			# Modes with no unlock mode, are never locked but can be completed
+			_is_completed = SaveManager.get_last_completed_level() >= mode.id_end - 1
+			_is_locked = false
 		PlayMode.UnlockMode.LEVEL:
 			message.text = tr("LEVEL_LOCK_MSG") % mode.unlock_count
-			_is_locked = SaveManager.get_start_level_playable() < mode.unlock_count
+
+			_is_completed = SaveManager.get_last_completed_level() >= (mode.id_end - 1)
+			_is_locked = SaveManager.get_last_completed_level() + 1 < mode.id_start - 1
+
 		PlayMode.UnlockMode.STAR:
+			# Modes unlocked with stars are never completed
 			message.text = tr("STAR_LOCK_MSG") % mode.unlock_count
 			_is_locked = SaveManager.get_star_count() < mode.unlock_count
 
 	if _is_locked:
 		overlay_texture.texture = load(LOCKED_MODE) as Texture2D
 		overlay_texture.show()
-	elif mode is StoryMode and SaveManager.get_start_level_playable() > mode.id_end - 1:
+	elif _is_completed:
 		overlay_texture.texture = load(COMPLETED_MODE) as Texture2D
 		message.text = tr("COMPLETED_MSG")
 		overlay_texture.show()
