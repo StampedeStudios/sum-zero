86bd934e5688a38742cf3a52b99953c734d7cc07
Simone <simonelungarella@gmail.com>
2026-02-01 15:57:10 +0100

    fix: improve panel animation by hiding contained and items before anim exec

--- scripts/user_interface/animated_panel.gd
+++ scripts/user_interface/animated_panel.gd
@@ -10,13 +10,23 @@ func _ready() -> void:
 
 
 func open() -> void:
-	var tween := create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
-	tween.tween_method(_animate, Vector2.ZERO, GameManager.ui_scale, anim_time)
+
+	for child: CanvasItem in get_children():
+		child.modulate.a = 0
+
+	var tween := create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
+	tween.tween_method(_animate, Vector2(GameManager.ui_scale.x, 0), GameManager.ui_scale, anim_time)
 	await tween.finished
+
+	await get_tree().create_timer(0.1).timeout
+
+	for child: CanvasItem in get_children():
+		tween = create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+		tween.tween_property(child, "modulate:a", 1.0, anim_time)
 
 
 func close() -> void:
-	scale = Vector2.ZERO
+	scale = Vector2(GameManager.ui_scale.x, 0)
 
 
 func _animate(animated_scale: Vector2) -> void:
