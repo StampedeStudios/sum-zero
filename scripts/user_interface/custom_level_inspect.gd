797eeaf9e37d1fb65a2d0b6fc37a45f035b6f2b7
Simone <simonelungarella@gmail.com>
2026-02-08 12:23:40 +0100

    fix: create base class for level inspection logic to reduce duplication

--- scripts/user_interface/custom_level_inspect.gd
+++ scripts/user_interface/custom_level_inspect.gd
@@ -1,108 +1,30 @@
-class_name CustomLevelInspect extends Control
+## UI for inspecting user-created or imported custom levels.
+class_name CustomLevelInspect extends BaseLevelInspect
 
 signal level_deleted
 
-const GAME_UI = "res://packed_scene/user_interface/GameUI.tscn"
-const LEVEL_BUILDER = "res://packed_scene/scene_2d/LevelBuilder.tscn"
-const BUILDER_UI = "res://packed_scene/user_interface/BuilderUI.tscn"
-const LEVEL_MANAGER = "res://packed_scene/scene_2d/LevelManager.tscn"
 const PASTE_CHECK_ICON = "res://assets/ui/paste_check_icon.png"
 
-var _level_id: int
 var _level_code: String
 
-@onready var label: Label = %LevelName
 @onready var delete_btn: Button = %DeleteBtn
-@onready var build_btn: Button = %BuildBtn
-@onready var play_btn: Button = %PlayBtn
 @onready var copy_btn: Button = %CopyBtn
-@onready var panel: AnimatedPanel = %Panel
-@onready var left_star: TextureRect = %LeftStar
-@onready var right_star: TextureRect = %RightStar
-@onready var middle_star: TextureRect = %MiddleStar
 
 
-func _ready() -> void:
-	right_star.scale = Vector2(0, 0)
-	left_star.scale = Vector2(0, 0)
-	middle_star.scale = Vector2(0, 0)
-
-	await panel.open()
+func _get_level_group() -> Constants.LevelGroup:
+	return Constants.LevelGroup.CUSTOM
 
 
-func close() -> void:
-	panel.close()
-	GameManager.change_state.call_deferred(Constants.GameState.LEVEL_PICK)
-	self.queue_free.call_deferred()
-
-
-func init_inspector(level_id: int, progress: LevelProgress) -> void:
+func _set_label_text(_lev_id: int, progress: LevelProgress) -> void:
 	label.text = progress.name
-	_level_id = level_id
 
-	var num_stars: int
-	if !progress.is_completed:
-		num_stars = 0
-	else:
-		num_stars = Constants.MAX_STARS_GAIN + progress.move_left
-		if num_stars < 0:
-			num_stars = 0
-		# extra reward for beating the developers (you think ...)
-		elif num_stars > 3:
-			num_stars = 4
-		update_stars(num_stars)
 
+## @param _progress The LevelProgress resource (not used in this implementation).
+func _post_init(_progress: LevelProgress) -> void:
 	GameManager.set_levels_context(Constants.LevelGroup.CUSTOM)
 	var level_data: LevelData = GameManager.get_active_level(_level_id)
 	_level_code = Encoder.encode(level_data)
 	copy_btn.text = " " + _level_code
-
-
-func _on_build_btn_pressed() -> void:
-	AudioManager.play_click_sound()
-	var scene := ResourceLoader.load(BUILDER_UI) as PackedScene
-	var builder_ui := scene.instantiate() as BuilderUI
-	get_tree().root.add_child.call_deferred(builder_ui)
-	GameManager.builder_ui = builder_ui
-
-	scene = ResourceLoader.load(LEVEL_BUILDER) as PackedScene
-	var level_builder := scene.instantiate() as LevelBuilder
-	get_tree().root.add_child.call_deferred(level_builder)
-	GameManager.level_builder = level_builder
-
-	GameManager.set_levels_context(Constants.LevelGroup.CUSTOM)
-	var level_data: LevelData = GameManager.get_active_level(_level_id)
-	level_builder.construct_level.call_deferred(level_data.duplicate(), true)
-
-	GameManager.change_state.call_deferred(Constants.GameState.BUILDER_IDLE)
-	self.queue_free.call_deferred()
-
-
-func _on_play_btn_pressed() -> void:
-	AudioManager.play_click_sound()
-	var scene := ResourceLoader.load(LEVEL_MANAGER) as PackedScene
-	var level_manager := scene.instantiate() as LevelManager
-	get_tree().root.add_child(level_manager)
-	GameManager.level_manager = level_manager
-
-	GameManager.set_levels_context(Constants.LevelGroup.CUSTOM)
-	var level_data: LevelData = GameManager.get_active_level(_level_id)
-	level_manager.init_level(level_data)
-
-	scene = ResourceLoader.load(GAME_UI) as PackedScene
-	var game_ui := scene.instantiate() as GameUI
-	get_tree().root.add_child(game_ui)
-	game_ui.initialize_ui(Constants.GameState.LEVEL_PICK)
-	GameManager.change_state(Constants.GameState.LEVEL_START)
-	GameManager.game_ui = game_ui
-
-	self.queue_free.call_deferred()
-
-
-func _on_background_gui_input(event: InputEvent) -> void:
-	if event is InputEventMouse and event.is_action_pressed(Literals.Inputs.LEFT_CLICK):
-		AudioManager.play_click_sound()
-		close()
 
 
 func _on_copy_btn_pressed() -> void:
@@ -116,14 +38,3 @@ func _on_delete_btn_pressed() -> void:
 	AudioManager.play_click_sound()
 	level_deleted.emit()
 	close()
-
-
-func update_stars(star_count: int) -> void:
-	if star_count >= 1:
-		left_star.scale = Vector2(0.7, 0.7)
-
-	if star_count >= 2:
-		middle_star.scale = Vector2(0.8, 0.8)
-
-	if star_count >= 3:
-		right_star.scale = Vector2(0.7, 0.7)
